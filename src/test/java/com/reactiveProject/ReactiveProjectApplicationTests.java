aefc9d2d519f09ac9280ededc8ecf871ac1afb0d
giorgio.depascale <giorgio.depascale@outlook.com>
2023-06-26 15:44:48 +0200

    first

---
+++ src/test/java/com/reactiveProject/ReactiveProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactiveProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

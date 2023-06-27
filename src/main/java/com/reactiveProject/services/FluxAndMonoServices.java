39dfb1c9d71590c14692140dbc6818bf73bd136f
giorgio.depascale <giorgio.depascale@outlook.com>
2023-06-27 11:01:56 +0200

    map, filter, filterMap

--- src/main/java/com/reactiveProject/services/FluxAndMonoServices.java
+++ src/main/java/com/reactiveProject/services/FluxAndMonoServices.java
@@ -7,22 +7,46 @@ import java.util.List;
 
 public class FluxAndMonoServices {
 
-    public Flux<String> fruitsFlux() {
-        return Flux.fromIterable(List.of("Apple", "Banana", "Orange")).log();
-    }
-
     public Mono<String> fruitMono() {
         return Mono.just("MonoApple").log();
     }
 
+    public Flux<String> fruitsFlux() {
+        return Flux.fromIterable(List.of("Apple", "Banana", "Orange")).log();
+    }
+
+    public Flux<String> fruitsFluxMap() {
+        return Flux.fromIterable(List.of("Apple", "Banana", "Orange"))
+                .map(String::toUpperCase);
+    }
+
+    public Flux<String> fruitsFluxFilter(int length) {
+        return Flux.fromIterable(List.of("Apple", "Banana", "Orange"))
+                .filter(fruit -> fruit.length() > length);
+    }
+
+    public Flux<String> fruitsFluxFilterMap(int length) {
+        return Flux.fromIterable(List.of("Apple", "Banana", "Orange"))
+                .filter(fruit -> fruit.length() > length)
+                .map(String::toUpperCase);
+    }
+
     public static void main(String[] args) {
+
         FluxAndMonoServices fluxAndMonoServices = new FluxAndMonoServices();
 
-        fluxAndMonoServices.fruitsFlux()
-            .subscribe(System.out::println);
-
         fluxAndMonoServices.fruitMono()
-            .subscribe(System.out::println);
+                .subscribe(System.out::println);
+
+        fluxAndMonoServices.fruitsFlux()
+                .subscribe(System.out::println);
+
+        fluxAndMonoServices.fruitsFluxMap()
+                .subscribe(System.out::println);
+
+        fluxAndMonoServices.fruitsFluxFilter(5)
+                .subscribe(System.out::println);
+
     }
 
 }
--- src/test/java/com/reactiveProject/services/FluxAndMonoServicesTest.java
+++ src/test/java/com/reactiveProject/services/FluxAndMonoServicesTest.java
@@ -8,6 +8,14 @@ class FluxAndMonoServicesTest {
     FluxAndMonoServices fluxAndMonoServices = new FluxAndMonoServices();
 
     @Test
+    void fruitMono() {
+        var fruitMono = fluxAndMonoServices.fruitMono();
+        StepVerifier.create(fruitMono)
+                .expectNext("MonoApple")
+                .verifyComplete();
+    }
+
+    @Test
     void fruitsFlux() {
         var fruitsFlux = fluxAndMonoServices.fruitsFlux();
         StepVerifier.create(fruitsFlux)
@@ -16,11 +24,26 @@ class FluxAndMonoServicesTest {
     }
 
     @Test
-    void fruitMono() {
-        var fruitMono = fluxAndMonoServices.fruitMono();
-        StepVerifier.create(fruitMono)
-            .expectNext("MonoApple")
+    void fruitsFluxMap() {
+        var fruitsFluxMap = fluxAndMonoServices.fruitsFluxMap();
+        StepVerifier.create(fruitsFluxMap)
+            .expectNext("APPLE", "BANANA", "ORANGE")
             .verifyComplete();
     }
 
+    @Test
+    void fruitsFluxFilter() {
+        var fruitsFluxFilter = fluxAndMonoServices.fruitsFluxFilter(5);
+        StepVerifier.create(fruitsFluxFilter)
+            .expectNext("Banana", "Orange")
+            .verifyComplete();
+    }
+
+    @Test
+    void fruitsFluxFilterMap() {
+        var fruitsFluxFilterMap = fluxAndMonoServices.fruitsFluxFilterMap(5);
+        StepVerifier.create(fruitsFluxFilterMap)
+            .expectNext("BANANA", "ORANGE")
+            .verifyComplete();
+    }
 }
